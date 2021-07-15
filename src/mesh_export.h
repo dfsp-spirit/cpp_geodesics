6b84ddf5d9f27b15bab18c3bd152313eba51a702
Tim Schäfer <ts+code@rcmd.org>
2021-07-15 22:25:47 +0200

    cleanups, better readability

--- src/mesh_export.h
+++ src/mesh_export.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include "typedef_vcg.h"
-#include <wrap/io_trimesh/export_ply.h> // for ExporterPLY t save to PLY format
-#include <wrap/ply/plylib.h> // for ExporterPLY t save#include <wrap/io_trimesh/import.h>
+#include <wrap/io_trimesh/export_ply.h>
+#include <wrap/ply/plylib.h>
 #include <vcg/complex/algorithms/pointcloud_normal.h>
 #include <wrap/io_trimesh/export_vrml.h>
 #include <wrap/io_trimesh/import.h>
@@ -10,5 +10,7 @@
 
 void export_mesh_ply(MyMesh& m, const std::string& outfile) {
     std::cout << "Exporting mesh in PLY format to file '" << outfile << "'.\n";
-    vcg::tri::io::ExporterPLY<MyMesh>::Save(m, outfile.c_str(), 0, false);
+    int mask = 0;   // The mask defines which mesh features to write (e.g., vertex colors, normals, ...)
+    bool use_binary_format_version = false;
+    vcg::tri::io::ExporterPLY<MyMesh>::Save(m, outfile.c_str(), mask, use_binary_format_version);
 }
