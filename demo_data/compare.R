ff69381d1fe7ea6a80db557f33bb03b6288fdad6
Tim Schäfer <ts+code@rcmd.org>
2021-07-22 14:44:36 +0200

    plot correlation

--- demo_data/compare.R
+++ demo_data/compare.R
@@ -7,14 +7,15 @@ library("freesurferformats");
 
 sjd = fsbrain::fsaverage.path(T);
 sj = "fsaverage3";
-do_vis = TRUE;
+do_vis = F;
 
 # Load results for fsaverage3 lh
 r_rad = freesurferformats::read.fs.curv("~/develop/cpp_geodesics/demo_data/R_output/lh.fsaverage3_radius_s5.curv");
 ml_rad = freesurferformats::read.fs.curv("~/develop/cpp_geodesics/demo_data/ml_output/lh.fsaverage3_radius_s5.curv");
 cpp_rad = freesurferformats::read.fs.curv("~/develop/cpp_geodesics/geocircles_radius.curv");
 
-cat(sprintf("[RADIUS] Range for R is %s, for ML is %s, for C++ is %s.\n", paste(range(r_rad), collapse=" "), paste(range(ml_rad), collapse=" "), paste(range(cpp_rad), collapse=" ")))
+cat(sprintf("[RADIUS] Range for R is %s, for ML is %s, for C++ is %s.\n", paste(range(r_rad), collapse=" "), paste(range(ml_rad), collapse=" "), paste(range(cpp_rad), collapse=" ")));
+cat(sprintf("[RADIUS] Correlaton with ML results is %f for R version, %f for C++version.\n", cor(r_rad, ml_rad), cor(cpp_rad, ml_rad)));
 if(do_vis) {
     fsbrain::vis.data.on.subject(sjd, sj, morph_data_lh = r_rad);
     fsbrain::vis.data.on.subject(sjd, sj, morph_data_lh = ml_rad);
@@ -27,7 +28,7 @@ ml_per = freesurferformats::read.fs.curv("~/develop/cpp_geodesics/demo_data/ml_o
 cpp_per = freesurferformats::read.fs.curv("~/develop/cpp_geodesics/geocircles_perimeter.curv");
 
 cat(sprintf("[PERIMETER] Range for R is %s, for ML is %s, for C++ is %s.\n", paste(range(r_per), collapse=" "), paste(range(ml_per), collapse=" "), paste(range(cpp_per), collapse=" ")))
-
+cat(sprintf("[PERIMETER] Correlaton with ML results is %f for R version, %f for C++version.\n", cor(r_per, ml_per), cor(cpp_per, ml_per)));
 if(do_vis) {
     fsbrain::vis.data.on.subject(sjd, sj, morph_data_lh = r_per);
     fsbrain::vis.data.on.subject(sjd, sj, morph_data_lh = ml_per);
