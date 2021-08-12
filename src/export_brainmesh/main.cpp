8e5a289f8b15d59c6275e195ca92cf7afb832750
Tim Schäfer <ts+code@rcmd.org>
2021-08-12 23:02:07 +0200

    Add export_brainmesh app

--- src/export_brainmesh/main.cpp
+++ src/export_brainmesh/main.cpp
@@ -16,11 +16,33 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include <chrono>
+
+
+/// Export a colored PLY brain mesh, can be viewed in Meshlab.
+void export_brain(const std::string& surf_file, const std::string& curv_file, const std::string& output_ply_file = "colored_brain.ply") {
+    // Load mesh and data.
+    fs::Mesh surface;
+    fs::read_mesh(&surface, surf_file);
+    std::vector<float> morph_data = fs::read_curv_data(curv_file);
+
+    // Map data to colors
+    std::vector<u_int8_t> colors = data_to_colors(morph_data);
+    surface.to_ply_file(output_ply_file, colors);
+    std::cout << "Vertex-colored brain mesh written to file '" << output_ply_file << "'.\n";    
+}
 
 
 int main(int argc, char** argv) {
 
- 
-
+    if(argc != 4) {
+        std::cout << "== Export colored brain mesh ==.\n";
+        std::cout << "Usage: " << argv[0] << " <surf_file> <curv_file> <output_ply_file>\n";
+        std::cout << "  <surf_file>       : path to a brain mesh file, typically in FreeSurfer surf format.\n";
+        std::cout << "  <curv_file>       : path to a file containing per-vertex data for the mesh, typically in FreeSurfer curv format.\n";
+        std::cout << "  <output_ply_file> : path to the output file in PLY format, will be created (or overwritten in case it exists).\n";
+        std::cout << "  Example: " << argv[0] << " demo_data/subjects_dir/subject1/surf/lh.white demo_data/subjects_dir/subject1/surf/lh.thickness colored_brain.ply\n";
+        exit(1);
+    }
+    export_brain(argv[1], argv[2], argv[3]);
+    exit(0);
 }
