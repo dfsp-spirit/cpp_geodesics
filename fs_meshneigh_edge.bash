5b404350af3dae60172965d1d2c75b687dbf7c47
Tim Schäfer <ts+code@rcmd.org>
2021-08-23 15:35:48 +0200

    finish geodesic precomp script

--- fs_meshneigh_edge.bash
+++ fs_meshneigh_edge.bash
@@ -1,19 +1,20 @@
 #!/bin/bash
-# Pre-compute mesh distances for FreeSurfder meshes and save them in JSON format.
+# Pre-compute mesh edge distances for FreeSurfer meshes and save them in JSON format.
 
 subjects_dir="$FREESURFER_HOME/subjects"
+apptag="[EDGE_DIST]"
 
 for subject in fsaverage fsaverage6; do
     if [ ! -d "${subjects_dir}/${subject}" ]; then
-      echo "ERROR: Cannot read directory '${subjects_dir}/${subject}', exiting."
+      echo "$apptag ERROR: Cannot read directory '${subjects_dir}/${subject}', exiting."
       exit 1
     fi
     for hemi in lh rh; do    
         for distance in 1 2 3 4 5; do
-            echo "Handling subject ${subject} hemi ${hemi} at distance ${distance}..."
+            echo "$apptag === Handling subject ${subject} hemi ${hemi} at distance ${distance}... ==="
             mesh_file="${subjects_dir}/${subject}/surf/${hemi}.white"
             if [ ! -f "${mesh_file}" ]; then
-                echo "ERROR: Cannot read mesh file '${mesh_file}', exiting."
+                echo "$apptag ERROR: Cannot read mesh file '${mesh_file}', exiting."
                 exit 1
             fi
             ./meshneigh_edge "${mesh_file}" ${distance} "${subject}_meshdist_edge_${distance}.json" true
@@ -21,6 +22,6 @@ for subject in fsaverage fsaverage6; do
     done
 done
 
-echo "All done, exiting."
+echo "$apptag All done, exiting."
 
 
---
+++ fs_meshneigh_geod.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Pre-compute mesh geodesic distances for FreeSurfer meshes and save them in JSON format.
+
+subjects_dir="$FREESURFER_HOME/subjects"
+apptag="[GEOD_DIST]"
+
+for subject in fsaverage fsaverage6; do
+    if [ ! -d "${subjects_dir}/${subject}" ]; then
+      echo "$apptag ERROR: Cannot read directory '${subjects_dir}/${subject}', exiting."
+      exit 1
+    fi
+    for hemi in lh rh; do    
+        for distance in 100; do
+            echo "$apptag === Handling subject ${subject} hemi ${hemi} at distance ${distance}... ==="
+            mesh_file="${subjects_dir}/${subject}/surf/${hemi}.white"
+            if [ ! -f "${mesh_file}" ]; then
+                echo "$apptag ERROR: Cannot read mesh file '${mesh_file}', exiting."
+                exit 1
+            fi
+            ./meshneigh_geod "${mesh_file}" ${distance} "${subject}_meshdist_geod_${distance}.json" true
+        done
+    done
+done
+
+echo "$apptag All done, exiting."
+
+
