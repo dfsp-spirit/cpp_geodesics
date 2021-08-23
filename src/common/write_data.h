6a587a53d636c480dad58a727be9a2b209fcadae
Tim Schäfer <ts+code@rcmd.org>
2021-08-23 16:42:20 +0200

    add binary format writer, JSON is way too verbose

---
+++ src/common/write_data.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+
+/// Determine the endianness of the system.
+///
+/// THIS FUNCTION IS INTERNAL AND SHOULD NOT BE CALLED BY API CLIENTS.
+bool _is_bigendian() {
+    short int number = 0x1;
+    char *numPtr = (char*)&number;
+    return (numPtr[0] != 1);
+}  
+
+/// Swap endianness of a value.
+///
+/// THIS FUNCTION IS INTERNAL AND SHOULD NOT BE CALLED BY API CLIENTS.
+template <typename T>
+T _swap_endian(T u) {
+    static_assert (CHAR_BIT == 8, "CHAR_BIT != 8");
+
+    union
+    {
+        T u;
+        unsigned char u8[sizeof(T)];
+    } source, dest;
+
+    source.u = u;
+
+    for (size_t k = 0; k < sizeof(T); k++)
+        dest.u8[k] = source.u8[sizeof(T) - k - 1];
+
+    return(dest.u);
+}
+
+/// Write a value to a stream as big endian.
+///
+/// THIS FUNCTION IS INTERNAL AND SHOULD NOT BE CALLED BY API CLIENTS.
+template <typename T>
+void _fwritet(std::ostream& os, T t) {
+    if(! _is_bigendian()) {
+        t = _swap_endian<T>(t);
+    }
+    os.write( reinterpret_cast<const char*>( &t ), sizeof(t));
+}
+
+/// Write vector of vectors to binary big endian file.
+template <typename T>
+void write_vv(const std::string& filename, std::vector<std::vector<T>> data) {
+    std::ofstream ofs;
+    ofs.open(filename, std::ofstream::out | std::ofstream::binary);
+    if(ofs.is_open()) {
+        _fwritet<int32_t>(ofs, 42); // write magic number 1
+        _fwritet<int32_t>(ofs, 13); // write magic number 2
+        _fwritet<int32_t>(ofs, data.size());
+        for(size_t i=0; i<data.size(); i++) {
+            _fwritet<int32_t>(ofs, data[i].size());
+            for(size_t j=0; j<data[i].size(); j++) {
+                _fwritet<T>(ofs, data[i][j]);
+            }
+        }
+      ofs.close();
+    } else {
+      throw std::runtime_error("Unable to open file '" + filename + "' for writing.\n");
+    }
+  }
