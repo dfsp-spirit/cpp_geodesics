2fe1b68c11f56589a3f6418ba8fa07f70d5065a4
Tim Schäfer <ts+code@rcmd.org>
2021-08-24 09:06:42 +0200

    add surface and hemi to precomp script

--- fs_meshneigh_edge.bash
+++ fs_meshneigh_edge.bash
@@ -9,15 +9,18 @@ for subject in fsaverage fsaverage6; do
       echo "$apptag ERROR: Cannot read directory '${subjects_dir}/${subject}', exiting."
       exit 1
     fi
-    for hemi in lh rh; do    
-        for distance in 1 2 3 4 5; do
-            echo "$apptag === Handling subject ${subject} hemi ${hemi} at distance ${distance}... ==="
-            mesh_file="${subjects_dir}/${subject}/surf/${hemi}.white"
-            if [ ! -f "${mesh_file}" ]; then
-                echo "$apptag ERROR: Cannot read mesh file '${mesh_file}', exiting."
-                exit 1
-            fi
-            ./meshneigh_edge "${mesh_file}" ${distance} "${subject}_meshdist_edge_${distance}" true
+    for hemi in lh rh; do
+        for surface in white; do    
+            for distance in 1 2 3 4 5; do
+                echo "$apptag === Handling subject ${subject} hemi ${hemi} surface ${surface} at distance ${distance}... ==="
+                mesh_file="${subjects_dir}/${subject}/surf/${hemi}.${surface}"
+                if [ ! -f "${mesh_file}" ]; then
+                    echo "$apptag ERROR: Cannot read mesh file '${mesh_file}', exiting."
+                    exit 1
+                fi
+                output_file="${subject}_${hemi}_${surface}_meshdist_edge_${distance}"
+                ./meshneigh_edge "${mesh_file}" ${distance} "${output_file}" true && echo "$apptag Edge results written to file '${output_file}'".
+            done
         done
     done
 done
--- fs_meshneigh_geod.bash
+++ fs_meshneigh_geod.bash
@@ -9,15 +9,18 @@ for subject in fsaverage6 fsaverage; do
       echo "$apptag ERROR: Cannot read directory '${subjects_dir}/${subject}', exiting."
       exit 1
     fi
-    for hemi in lh rh; do    
-        for distance in 100; do
-            echo "$apptag === Handling subject ${subject} hemi ${hemi} at distance ${distance}... ==="
-            mesh_file="${subjects_dir}/${subject}/surf/${hemi}.white"
-            if [ ! -f "${mesh_file}" ]; then
-                echo "$apptag ERROR: Cannot read mesh file '${mesh_file}', exiting."
-                exit 1
-            fi
-            ./meshneigh_geod "${mesh_file}" ${distance} "${subject}_meshdist_geod_${distance}" true
+    for hemi in lh rh; do
+        for surface in white; do
+            for distance in 55; do
+                echo "$apptag === Handling subject ${subject} hemi ${hemi} at distance ${distance}... ==="
+                mesh_file="${subjects_dir}/${subject}/surf/${hemi}.${surface}"
+                if [ ! -f "${mesh_file}" ]; then
+                    echo "$apptag ERROR: Cannot read mesh file '${mesh_file}', exiting."
+                    exit 1
+                fi
+                output_file="${subject}_${hemi}_${surface}_meshdist_geod_${distance}"
+                ./meshneigh_geod "${mesh_file}" ${distance} "${output_file}" true && echo "$apptag Geodesic results written to file '${output_file}'".
+            done
         done
     done
 done
