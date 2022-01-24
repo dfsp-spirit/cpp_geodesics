db4e55f6540e8235e55607e40943784e4ff9558c
Tim Schaefer <ts+code@rcmd.org>
2022-01-24 16:37:39 +0100

    add file_exists function

---
+++ src/common/io.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+inline bool file_exists (const std::string& name) {
+    if (FILE *file = fopen(name.c_str(), "r")) {
+        fclose(file);
+        return true;
+    } else {
+        return false;
+    }   
+}
