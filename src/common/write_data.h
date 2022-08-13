de0eff03ad37b55f46df92340c9f2711297b6b40
Tim Schäfer <ts+code@rcmd.org>
2022-08-13 21:55:19 +0200

    NEW: better doc strings for write_data.h functions

--- src/common/write_data.h
+++ src/common/write_data.h
@@ -5,18 +5,22 @@
 #include <iostream>
 #include <fstream>
 
-/// Determine the endianness of the system.
+/// @brief Determine the endianness of the system.
 ///
 /// THIS FUNCTION IS INTERNAL AND SHOULD NOT BE CALLED BY API CLIENTS.
+/// @return bool, whether system is big endian.
+/// @private
 bool _is_bigendian() {
     short int number = 0x1;
     char *numPtr = (char*)&number;
     return (numPtr[0] != 1);
-}  
+}
 
-/// Swap endianness of a value.
+/// @brief Swap endianness of a value.
 ///
 /// THIS FUNCTION IS INTERNAL AND SHOULD NOT BE CALLED BY API CLIENTS.
+/// @return value with swapped endianness
+/// @private
 template <typename T>
 T _swap_endian(T u) {
     static_assert (CHAR_BIT == 8, "CHAR_BIT != 8");
@@ -35,9 +39,11 @@ T _swap_endian(T u) {
     return(dest.u);
 }
 
-/// Write a value to a stream as big endian.
+/// @brief Write a value to a stream as big endian.
 ///
 /// THIS FUNCTION IS INTERNAL AND SHOULD NOT BE CALLED BY API CLIENTS.
+/// @return nothing, called for side effect of writing to stream.
+/// @private
 template <typename T>
 void _fwritet(std::ostream& os, T t) {
     if(! _is_bigendian()) {
@@ -46,14 +52,21 @@ void _fwritet(std::ostream& os, T t) {
     os.write( reinterpret_cast<const char*>( &t ), sizeof(t));
 }
 
+/// @brief The VV format default magic header string, used for 'float_32' data.
+/// @private
+/// @return the type code 14
 template <typename T>
 int32_t _vv_data_type_code() { return(14); }
 
+
+/// @brief The VV format magic header string meaning that the data is of 'int_32' datatype.
+/// @private
+/// @return the type code 13
 template <int32_t>
 int32_t _vv_data_type_code() { return(13); }
 
-/// Write vector of vectors to binary big endian file.
-/// The format is: 2 magic int32 numbers followed by size of outer vec (also as int32). Then, for each inner vec: int32 size of vec, then the values.
+/// @brief Write vector of vectors to file, using big endian byte order.
+/// @details The VV format is: 2 magic int32 numbers followed by size of outer vec (also as int32). Then, for each inner vec: int32 size of vec, then the values.
 template <typename T>
 void write_vv(const std::string& filename, std::vector<std::vector<T>> data) {
     std::ofstream ofs;
