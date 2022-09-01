2b613ffebd79e2f3b3a1676bb8517ef1e67e99da
Tim Schäfer <ts+code@rcmd.org>
2022-09-01 20:55:33 +0200

    NEW: add warning on carefully chosing distance param for mesh_geodneigh_edge

--- fs_meshneigh_geod.bash
+++ fs_meshneigh_geod.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 # Pre-compute mesh geodesic distances for FreeSurfer meshes and save them to files.
+#
+# IMPORTANT: The distance you chose below must fit to the mesh! Note that for FreeSurfer meshes,
+#            the suitable distance differs based on the mesh ICO value (simplification factor of the
+#            mesh), as meshes with fewer vertices (like fsaverage6 compared to the full fsaverage) will
+#            have longer edges! Running geodesic neighborhood computation with a too low 'distance' value
+#            means that the neighborhoods will be empty (or very small), because no (or only very few) vertices
+#            will be found within the max query distance!
 
 subjects_dir="$FREESURFER_HOME/subjects"
 apptag="[GEOD_DIST]"
