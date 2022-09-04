94346dd8964f5d8785d72220de7aa5e802696ae2
Tim Schäfer <ts+code@rcmd.org>
2022-09-04 14:06:59 +0200

    NEW: implement support for writing label to CSV into edge neigh bash script

--- fs_meshneigh_edge.bash
+++ fs_meshneigh_edge.bash
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Pre-compute mesh edge distances for FreeSurfer meshes and save them in JSON format.
 
+### Settings
+
 subjects_dir="$SUBJECTS_DIR"
 apptag="[EDGE_DIST_BASH]"
-
 subjects_list="fsaverage fsaverage6"
-
 use_subjects_file="true"  # Set to "false" or "true".
-
 subjects_file="${subjects_dir}/subjects.txt"
+pvd_descriptor="pial_lgi" # Set to something like "thickness", "volume", "area", or the empty string "" for none.
+
+### End of Settings
 
 if [ "${use_subjects_file}" = "true" ]; then
     if [ ! -f "${subjects_file}"  ]; then
@@ -36,6 +38,9 @@ extra_args="${include_self} ${json} ${csv} ${vv} ${with_neigh}"
 echo "$apptag INFO: Using meshneigh_edge extra_args '$extra_args'."
 echo "$apptag INFO: Handling ${num_subjects} subjects${log_tag_from}."
 
+
+
+
 num_handled=0
 for subject in ${subjects_list}; do
     num_handled=$((num_handled+1))
@@ -53,7 +58,13 @@ for subject in ${subjects_list}; do
                     exit 1
                 fi
                 output_file="${subject}_${hemi}_${surface}_meshdist_edge_${distance}"
-                ./meshneigh_edge "${mesh_file}" ${distance} "${output_file}" ${extra_args} && echo "$apptag Edge results for hemi $hemi surface $surface distance $distance written to file '${output_file}'".
+
+                pvd_descriptor_file=""
+                if [ -n "${pvd_descriptor}" ]; then
+                    pvd_descriptor_file="${subjects_dir}/${subject}/surf/${hemi}.${pvd_descriptor}"
+                fi
+
+                ./meshneigh_edge "${mesh_file}" ${distance} "${output_file}" ${extra_args} ${pvd_descriptor_file} && echo "$apptag Edge results for hemi $hemi surface $surface distance $distance written to file '${output_file}'".
             done
         done
     done
