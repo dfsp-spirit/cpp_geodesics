5aea84529b26714bd1301a646cef53188aa64fa7
Tim Schäfer <ts+code@rcmd.org>
2022-09-04 21:07:05 +0200

    WIP: prepare running wrapper bash script in parallel

--- fs_meshneigh_edge.bash
+++ fs_meshneigh_edge.bash
@@ -39,36 +39,43 @@ echo "$apptag INFO: Using meshneigh_edge extra_args '$extra_args'."
 echo "$apptag INFO: Handling ${num_subjects} subjects${log_tag_from}."
 
 
+run_with_gnu_parallel="true"
 
+if [ "${run_with_gnu_parallel}" = "true" ]; then
 
-num_handled=0
-for subject in ${subjects_list}; do
-    num_handled=$((num_handled+1))
-    if [ ! -d "${subjects_dir}/${subject}" ]; then
-      echo "$apptag ERROR: Cannot read directory '${subjects_dir}/${subject}', exiting."
-      exit 1
-    fi
-    for hemi in lh rh; do
-        for surface in pial; do
-            for distance in 5; do
-                echo "$apptag === Handling subject #${num_handled} of ${num_subjects} named '${subject}': hemi ${hemi} surface ${surface} at distance ${distance}... ==="
-                mesh_file="${subjects_dir}/${subject}/surf/${hemi}.${surface}"
-                if [ ! -f "${mesh_file}" ]; then
-                    echo "$apptag ERROR: Cannot read mesh file '${mesh_file}', exiting."
-                    exit 1
-                fi
-                output_file="${subject}_${hemi}_${surface}_meshdist_edge_${distance}"
+    echo "$APPTAG Running in parallel with `GNU Parallel`."
+else
+    echo "$APPTAG Running sequentially."
 
-                pvd_descriptor_file=""
-                if [ -n "${pvd_descriptor}" ]; then
-                    pvd_descriptor_file="${subjects_dir}/${subject}/surf/${hemi}.${pvd_descriptor}"
-                fi
+    num_handled=0
+    for subject in ${subjects_list}; do
+        num_handled=$((num_handled+1))
+        if [ ! -d "${subjects_dir}/${subject}" ]; then
+        echo "$apptag ERROR: Cannot read directory '${subjects_dir}/${subject}', exiting."
+        exit 1
+        fi
+        for hemi in lh rh; do
+            for surface in pial; do
+                for distance in 5; do
+                    echo "$apptag === Handling subject #${num_handled} of ${num_subjects} named '${subject}': hemi ${hemi} surface ${surface} at distance ${distance}... ==="
+                    mesh_file="${subjects_dir}/${subject}/surf/${hemi}.${surface}"
+                    if [ ! -f "${mesh_file}" ]; then
+                        echo "$apptag ERROR: Cannot read mesh file '${mesh_file}', exiting."
+                        exit 1
+                    fi
+                    output_file="${subject}_${hemi}_${surface}_meshdist_edge_${distance}"
 
-                ./meshneigh_edge "${mesh_file}" ${distance} "${output_file}" ${extra_args} ${pvd_descriptor_file} && echo "$apptag Edge results for hemi $hemi surface $surface distance $distance written to file '${output_file}'".
+                    pvd_descriptor_file=""
+                    if [ -n "${pvd_descriptor}" ]; then
+                        pvd_descriptor_file="${subjects_dir}/${subject}/surf/${hemi}.${pvd_descriptor}"
+                    fi
+
+                    ./meshneigh_edge "${mesh_file}" ${distance} "${output_file}" ${extra_args} ${pvd_descriptor_file} && echo "$apptag Edge results for hemi $hemi surface $surface distance $distance written to file '${output_file}'".
+                done
             done
         done
     done
-done
+fi
 
 echo "$apptag INFO: All ${num_subjects} subjects${log_tag_from} done, exiting."
 
