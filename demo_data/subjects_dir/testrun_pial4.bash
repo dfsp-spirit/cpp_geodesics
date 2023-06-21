2baed2956ec3e2188e956adeac001b86bd2bc906
Tim Schaefer <ts+code@rcmd.org>
2023-06-21 09:12:42 +0200

    NEW: support other surfaces

--- demo_data/subjects_dir/testrun_pial4.bash
+++ demo_data/subjects_dir/testrun_pial4.bash
@@ -1,11 +1,35 @@
 #!/bin/bash
 #
 # Runs a quick test of the geodcircles program on an ICO4 mesh, and exports the resulting overlays to colored meshes in PLY format.
+#
+# Make sure you have compiled the geodcircles program and the export_brainmesh program.
+# Run this from the directory it is stored in:
+#
+#  ./testrun_pial4.bash
+#
+# or:
+#
+#  ./testrun_pial4.bash pialsurface5
 
+APPTAG="[TESTRUN]"
 
-../../geodcircles subjects.txt . pialsurface4 2 1 2
-../../export_brainmesh subject1/surf/lh.pialsurface4 subject1/surf/lh.geocirc_perimeter_vcglib_pialsurface4_circscale2.curv lh.pial4_perimeter.ply
-../../export_brainmesh subject1/surf/lh.pialsurface4 subject1/surf/lh.geocirc_radius_vcglib_pialsurface4_circscale2.curv lh.pial4_radius.ply
-../../export_brainmesh subject1/surf/lh.pialsurface4 subject1/surf/lh.mean_geodist_vcglib_pialsurface4.curv lh.pial4_meandist.ply
+if [ -n "$1" ]; then
+    surface=$1
+    if [ "${surface}" == "pialsurface4" -o "${surface}" == "pialsurface5" -o "${surface}" == "pialsurface6"]; then
+        echo "${APPTAG} Using surface ${surface} from command line."
+    else
+        echo "${APPTAG} ERROR: Parameter 'surface' must be 'pialsurface4', 'pialsurface5', or 'pialsurface6'."
+        echo "${APPTAG} USAGE: $0 [surface]"
+        exit 1
+    fi
+else
+    surface="pialsurface4"
+    echo "${APPTAG} Using surface ${surface}."
+fi
+
+../../geodcircles subjects.txt . ${surface} 2 1 2
+../../export_brainmesh subject1/surf/lh.${surface} subject1/surf/lh.geocirc_perimeter_vcglib_${surface}_circscale2.curv lh.${surface}_perimeter.ply
+../../export_brainmesh subject1/surf/lh.${surface} subject1/surf/lh.geocirc_radius_vcglib_${surface}_circscale2.curv lh.${surface}_radius.ply
+../../export_brainmesh subject1/surf/lh.${surface} subject1/surf/lh.mean_geodist_vcglib_${surface}.curv lh.${surface}_meandist.ply
 
 
