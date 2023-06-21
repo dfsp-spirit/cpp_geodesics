3e691f11df211d110e0d0e72b5fe92aa08c5c666
Tim Schaefer <ts+code@rcmd.org>
2023-06-21 10:17:36 +0200

    FIX: set SUBJECTS_DIR, sanity checks

--- demo_data/subjects_dir/downsample_label.bash
+++ demo_data/subjects_dir/downsample_label.bash
@@ -3,9 +3,24 @@
 # The downsampled label can be used to find the cortical vertices (as opposed to the medial wall) for a subject on its ico6 surface.
 # Usage: bash downsample_label.bash
 
+APPTAG="[DS_LABEL]"
 SUBJECT="subject1"
 LABEL="cortex"
 ICO_ORDER=6
 
-mri_label2label --srclabel ./${SUBJECT}/label/lh.cortex.label --srcsubject "${SUBJECT}" --trglabel ./${SUBJECT}/label/lh.cortex${ICO_ORDER}.label --trgsubject ico --regmethod surface --hemi lh --trgicoorder ${ICO_ORDER}
-mri_label2label --srclabel ./${SUBJECT}/label/rh.cortex.label --srcsubject "${SUBJECT}" --trglabel ./${SUBJECT}/label/rh.cortex${ICO_ORDER}.label --trgsubject ico --regmethod surface --hemi rh --trgicoorder ${ICO_ORDER}
+export SUBJECTS_DIR=$(pwd)
+
+if [ ! -d "${SUBJECT}" ]; then
+    echo "${APPTAG} ERROR: Directory '${SUBJECT}' not found. Please run this script from the directory it is stored in."
+    exit 1
+fi
+
+lh_output_label="./${SUBJECT}/label/lh.${LABEL}${ICO_ORDER}.label"
+rh_output_label="./${SUBJECT}/label/rh.${LABEL}${ICO_ORDER}.label"
+mri_label2label --srclabel ./${SUBJECT}/label/lh.${LABEL}.label --srcsubject "${SUBJECT}" --trglabel "${lh_output_label}" --trgsubject ico --regmethod surface --hemi lh --trgicoorder ${ICO_ORDER}
+mri_label2label --srclabel ./${SUBJECT}/label/rh.${LABEL}.label --srcsubject "${SUBJECT}" --trglabel "${rh_output_label}" --trgsubject ico --regmethod surface --hemi rh --trgicoorder ${ICO_ORDER}
+
+echo "${APPTAG} Done. Check for error messages above."
+echo "${APPTAG} If everything went fine, labels were written to:"
+echo "${APPTAG}  * lh: ${lh_output_label}"
+echo "${APPTAG}  * rh: ${rh_output_label}"
